ef5066bc8feff8fa18c6506c600e54778cddb787
Qwerty <houldcould78@mail.ru>
2025-09-27 19:21:40 +0300

    text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,39 +22,38 @@ export default function RootLayout({
           name="keywords"
           content="люстра потолочная, люстра подвесная купить, учильные светильники moreelecktiki, moreelecktiki"
         />
-        {/* Метатег для Яндекс.Верификации */}
         <meta name="yandex-verification" content="0747f13ec82aa5" />
       </Head>
 
-      {/* Яндекс.Метрика */}
-      <Script id="yandex-metrika" strategy="afterInteractive">
-        {`
-          (function(m,e,t,r,i,k,a){
-              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-          })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=104303280', 'ym');
-
-          ym(104303280, 'init', {
-              ssr: true,
-              webvisor: true,
-              clickmap: true,
-              ecommerce: "dataLayer",
-              accurateTrackBounce: true,
-              trackLinks: true
-          });
-        `}
-      </Script>
-
-      {/* <noscript> для Яндекс.Метрики */}
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<div><img src="https://mc.yandex.ru/watch/104303280" style="position:absolute; left:-9999px;" alt="" /></div>`,
-        }}
-      />
-
       <body className={inter.className}>
+        {/* Яндекс.Метрика скрипт */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=104303280', 'ym');
+
+            ym(104303280, 'init', {
+                ssr: true,
+                webvisor: true,
+                clickmap: true,
+                ecommerce: "dataLayer",
+                accurateTrackBounce: true,
+                trackLinks: true
+            });
+          `}
+        </Script>
+
+        {/* ✅ Теперь noscript внутри body */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<div><img src="https://mc.yandex.ru/watch/104303280" style="position:absolute; left:-9999px;" alt="" /></div>`,
+          }}
+        />
+
         <AdBlockWarning />
         <main>
           <Header />
